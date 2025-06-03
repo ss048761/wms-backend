fffe953ac20d7052459ec48a1ac0913d3fe3ad1a
ss048761 <ss048761@gmail.com>
2025-06-03 18:34:44 +0530

    Removed wms import and urls

--- backend/urls.py
+++ backend/urls.py
@@ -12,6 +12,7 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
+    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('admin/', admin.site.urls),
 
     # JWT Token Endpoints
